7cd4d5035b4850aaa24623c29baed6a2a84043b1
Zcy <157526994+Zyneeee@users.noreply.github.com>
2024-07-02 21:50:25 +0700

    Update api.py

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 with open('pickup-lines.json', 'r') as f:
     picku-plines_data = json.load(f)
 
-pickup-lines = pickup-lines_data['pickup-lines']
+pickup-lines == pickup-lines_data['pickup-lines']
 
 @app.route('/api/random/pickup?lines', methods=['GET'])
 def get_pickup():
